5911290b665bd25b5cd22adcebf5db40af83e61f
Rahul <rk507922@gmail.com>
2021-08-14 01:36:50 +0530

    updates

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from "@angular/material/radio";
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatRadioModule,
+  ]
+})
+export class AuthModule { }
